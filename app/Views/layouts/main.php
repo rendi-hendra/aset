77870ba97054d8a79c091ba6e94c6602a6738056
Rendi Hendra Syahputra <babehndr5@gmail.com>
2026-02-11 10:50:30 +0700

    Update UI

---
+++ app/Views/layouts/main.php
@@ -0,0 +1,5 @@
+<?= $this->include('layouts/header') ?>
+<main>
+    <?= $this->renderSection('content') ?>
+</main>
+<?= $this->include('layouts/footer') ?>
---
+++ app/Views/layouts/sidebar.php
@@ -0,0 +1,31 @@
+<?php $uri = service('uri'); ?>
+
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-text mx-3">Admin</div>
+    </a>
+
+    <hr class="sidebar-divider my-0">
+
+    <!-- Dashboard -->
+    <li class="nav-item  <?= ($uri->getSegment(1) == '') ? 'active' : '' ?>">
+        <a class="nav-link" href="<?= base_url('/') ?>">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <!-- User -->
+    <li class="nav-item <?= ($uri->getSegment(1) == 'user') ? 'active' : '' ?>">
+        <a class="nav-link" href="<?= base_url('/user') ?>">
+            <i class="bi bi-person-fill"></i>
+            <span>User</span>
+        </a>
+    </li>
+    <hr class="sidebar-divider d-none d-md-block">
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
